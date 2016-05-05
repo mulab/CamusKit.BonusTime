9b695beebff4bac7f796187cabbe0d2dbd602502
Songzhou Yang <yangsongzhou@outlook.com>
2016-05-05 23:00:02 +0800

    delete some keywords

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,9 +8,9 @@ $(document).ready(function () {
     $body.css('width', $window.innerWidth()).css('height', $window.innerHeight());
   });
 
-  var persons = ['和风', '细雨', '暖阳', '烈日', '大雾', '大风', '暴雨', '雷雨', '多云', '冰雹'];
-  var places = ['渤海', '黄海', '东海', '南海', '黑海', '红海', '里海', '北冰洋', '加勒比', '池塘'];
-  var things = ['帆船', '游轮', '货轮', '战舰', '油船', '散货船', '集装箱船', '塑料板', '竹筏', '救生圈'];
+  var persons = ['和风', '细雨', '暖阳', '烈日', '大雾', '大风', '多云', '冰雹'];
+  var places = ['渤海', '黄海', '东海', '南海', '黑海', '红海', '里海', '后海', '前海'];
+  var things = ['帆船', '游轮', '货轮', '战舰', '油船', '散货船', '塑料板', '竹筏', '救生圈'];
   var intervalFlag = 0, interval1 = 0, interval2 = 0, interval3 = 0;
 
   $body.keydown(function (event) {
